622839994ffda1eff6433ca8842268bed45bb020
masao8231 <mm.neto2007@gmail.com>
2021-10-09 18:36:56 -0300

    feito o layout do menu

--- projeto/main.cpp
+++ projeto/main.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <iostream>
 #include "includes/funcoes.h"
-
+#include <vector>
 #include <time.h>
 
 using namespace std;
@@ -18,13 +18,24 @@ struct peca
 	int largura; 
 	int estoque; 
 };
+void reproducao(){
+
+}
+
+
+void mutacao(){
+
+}
+
 
 void crossover(){
 
 }
 
+//a função que vai lidar com o problema em si
+//a ideia será fazer o programa gerado pela GP definir o número de cortes em função da largura e demanda definida
 void fitness(){
-
+	
 }
 
 arvoregenes * populacaoInicial(int qnt, int maxlv){
@@ -51,12 +62,45 @@ arvoregenes * populacaoInicial(int qnt, int maxlv){
 arvoregenes individuos[40]; 
 
 int main(){
-	saida out; 
-	Funcoes f; 
-	
-	arvoregenes * teste; 
-	teste = populacaoInicial(7, 3);
-	cout << "\n";
+	int opt;
+	bool on = true; 
+
+	while (on)
+	{
+		cout << "Selecione uma opção\n";
+		cout << "1- Gerar população incial\n";
+		cout << "2- Testar população\n";
+		cout << "3- Treinar População (irá gerar uma nova geração)\n";
+		cout << "4- Sair\n";
+
+		cin >> opt;
+
+		switch (opt)
+		{
+		case 1:
+			cout << "\nGerando população inicial...\n";
+			break;
+		
+		case 2:
+			cout << "\nListando cada saída do problema da população\n";
+			break;
+		
+		case 3:
+			cout << "\nTreinando População\n";
+			break;
+
+		case 4:
+			cout << "\nSaindo...\n";
+			on = false; 
+			break;
+
+		default:
+			cout << "\nopção incorreta, por favor digite o que está solicitado no menu.\n";
+			break;
+		}
+
+	}
+	 
 
 	return 0;
 }
